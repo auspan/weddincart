6017ce18b97b9d48e8e42dea825212d6c528c2f8
Utkal <utkal.pande@gmail.com>
2017-01-11 17:20:54 +0530

    Added faq answers and know more popup

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -6,42 +6,47 @@
         <ul class="mtree transit" id="faqs">
             <li><a href="#"> 1. Account Activation/Sign Up </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#"> Starting with Wedding Cart is really simple. To start your wedding registry go to the sign-up page and register with your details. Once registered, you will be asked to create your couple profile. You can put pictures and personalized messages for your guests. You are ready to start inviting your guests.</a></li>
                 </ul>
             </li>
             <li><a href="#"> 2. How can I set up my wedding page? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#">As you create your profile you wedding page is getting created in the back ground. All you need to do from your side is to follow the flow as instructed. </a></li>
                 </ul>
             </li>
             <li><a href="#"> 3. How can I create my wedding gift wish list </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#">For your convenience, we have already created basic household items your wish list. If you want to add any additional items then simple click on the “add more” button at the end of the page. You can click on the custom option on pop-up window. You can delete any line items if you do not wish to keep them. Please note that all item description and images can be changed as per your wish.</a></li>
                 </ul>
             </li>
             <li><a href="#"> 4. How can I create an online invitation and share it? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#">Once your wedding details are created (couple profile, events page and wish list)you can move ahead and send invites to your guests. We have provided and option of downloading contacts from your email services or social media sites. Once you have captured you can simply click and choose the contact emails that you would like to send the invites. Put a personalized message and send it. </a></li>
                 </ul>
             </li>
             <li><a href="#"> 5. Can I add/modify my wish list after I have published it? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#">Yes, You can login and add/modify your wishlist anytime you want up till you have received a contribution against it. </a></li>
                 </ul>
             </li>
             <li><a href="#"> 6. How will I know that I have got a contribution? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#"> You will receive a SMS in your registered and updates via email when a contribution is made.  </a></li>
                 </ul>
             </li>
             <li><a href="#"> 7. How will I get my gifts? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#"> Our WeddinCart Account Manager will start engaging with you during the final weeks of your wedding date to discuss procurement and delivery of the gifts. Small gift items can be delivered at your home, or at the marriage venue. You can ask for it to be delivered before or after the marriage. </a></li>
                 </ul>
             </li>
             <li><a href="#"> 8. What are the charges for cash contributions? </a>
                 <ul>
-                    <li><a href="#"> Answer to be displayed here </a></li>
+                    <li><a href="#"> We have standard charges which are 8% of the total contribution value. We have standard charges for cash or We charge a nominal fee to maintain and upgrade your portal and the price is also inclusive of VAT, our partners for payment gateway, etc. </a></li>
+                </ul>
+            </li>
+            <li><a href="#"> 9. Are there any different charge for cash contributions?</a>
+                <ul>
+                    <li><a href="#">We don’t have the provision for accepting cash contributions at the moment.</a></li>
                 </ul>
             </li>
         </ul>
---
+++ resources/views/pages/knowMorePopup.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="overlay">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+                <p>Remember to register your details to get information about the launch.</p>
+            </div>
+            <div class="modal-footer">
+                <button onclick="window.location.href='register.php'" class="hvr-bounce-to-right btn2" data-dismiss="modal">Register Now</button>
+            </div>
+        </div>
+    </div>
+</div>
