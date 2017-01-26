5c22ab6fa1a200aa62f65d6fe31d06c36009d526
Utkal <utkal.pande@gmail.com>
2017-01-26 14:19:15 +0530

    Update social media links

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -8,10 +8,10 @@
             <div class="container">
                 <div class="social-icons">
                     <ul class="">
-                        <li><a href="#" target="_self"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_self"><i class="fa fa-tumblr" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_self"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_self"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/weddincartcom-355712654802860" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.instagram.com/weddincart/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        {{--<li><a href="#" target="_self"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>--}}
+                        <li><a href="https://twitter.com/weddincart/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                     </ul>
                 </div>
             </div>
